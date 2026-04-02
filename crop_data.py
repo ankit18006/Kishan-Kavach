fbdf56889c957d5a71c0bccc43ba805bc473a992
Ankit Shukla <163987630+ankit18006@users.noreply.github.com>
2026-04-02 10:42:27 +0530

    Create crop_data.py

---
+++ crop_data.py
@@ -0,0 +1,26 @@
+CROP_DATA = {
+    "Potato": {
+        "temp_min": 4,
+        "temp_max": 10,
+        "humidity_min": 85,
+        "humidity_max": 95,
+        "gas": "CO2",
+        "shelf_life_days": 60
+    },
+    "Tomato": {
+        "temp_min": 12,
+        "temp_max": 20,
+        "humidity_min": 85,
+        "humidity_max": 90,
+        "gas": "Ethylene",
+        "shelf_life_days": 10
+    },
+    "Onion": {
+        "temp_min": 0,
+        "temp_max": 4,
+        "humidity_min": 60,
+        "humidity_max": 70,
+        "gas": "Ammonia",
+        "shelf_life_days": 90
+    }
+}
